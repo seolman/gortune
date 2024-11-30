473070692776439d09b09d5c65582e9d95d34c98
seolman <tjfehdgns@gmail.com>
2024-11-30 22:24:20 +0900

    Fix: assets

--- main.go
+++ main.go
@@ -1,68 +1,68 @@
 package main
 
 import (
-	"embed"
-	"fmt"
-	"log"
-	"math/rand"
-	"strings"
-	"time"
+    "embed"
+    "fmt"
+    "log"
+    "math/rand"
+    "strings"
+    "time"
 )
 
 //go:embed cookies/*
 var cookies embed.FS
 
 func main() {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
+    rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	dir, err := cookies.ReadDir("cookies")
-	if err != nil {
-		log.Fatal(err)
-	}
+    dir, err := cookies.ReadDir("cookies")
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	var fortunes []string
+    var fortunes []string
 
     // WARN: not scalable
-	for _, d := range dir {
-		if d.Type().IsDir() {
-			sub, err := cookies.ReadDir("cookies/" + d.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, s := range sub {
-				data, err := cookies.ReadFile("cookies/" + d.Name() + "/" + s.Name())
-				if err != nil {
-					log.Fatal(err)
-				}
-				content := string(data)
-				phrases := strings.Split(content, "%")
-				for _, phrase := range phrases {
-					trimmedPhrase := strings.TrimSpace(phrase)
-					if trimmedPhrase != "" {
-						fortunes = append(fortunes, trimmedPhrase)
-					}
-				}
-			}
-		} else {
-			data, err := cookies.ReadFile("cookies/" + d.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-			content := string(data)
-			phrases := strings.Split(content, "%")
-			for _, phrase := range phrases {
-				trimmedPhrase := strings.TrimSpace(phrase)
-				if trimmedPhrase != "" {
-					fortunes = append(fortunes, trimmedPhrase)
-				}
-			}
-		}
-	}
+    for _, d := range dir {
+        if d.Type().IsDir() {
+            sub, err := cookies.ReadDir("cookies/" + d.Name())
+            if err != nil {
+                log.Fatal(err)
+            }
+            for _, s := range sub {
+                data, err := cookies.ReadFile("cookies/" + d.Name() + "/" + s.Name())
+                if err != nil {
+                    log.Fatal(err)
+                }
+                content := string(data)
+                phrases := strings.Split(content, "%")
+                for _, phrase := range phrases {
+                    trimmedPhrase := strings.TrimSpace(phrase)
+                    if trimmedPhrase != "" {
+                        fortunes = append(fortunes, trimmedPhrase)
+                    }
+                }
+            }
+        } else {
+            data, err := cookies.ReadFile("cookies/" + d.Name())
+            if err != nil {
+                log.Fatal(err)
+            }
+            content := string(data)
+            phrases := strings.Split(content, "%")
+            for _, phrase := range phrases {
+                trimmedPhrase := strings.TrimSpace(phrase)
+                if trimmedPhrase != "" {
+                    fortunes = append(fortunes, trimmedPhrase)
+                }
+            }
+        }
+    }
 
-	if len(fortunes) > 0 {
-		randomIndex := rand.Intn(len(fortunes))
-		fmt.Println(fortunes[randomIndex])
-	} else {
-		fmt.Println("No fortunes found.")
-	}
+    if len(fortunes) > 0 {
+        randomIndex := rand.Intn(len(fortunes))
+        fmt.Println(fortunes[randomIndex])
+    } else {
+        fmt.Println("No fortunes found.")
+    }
 }
